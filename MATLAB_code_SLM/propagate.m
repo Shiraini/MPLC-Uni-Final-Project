c2e4046d7c0df57dea9510e4df7f9c0f2ba48ad2
Shiraini <68999129+Shiraini@users.noreply.github.com>
2025-07-16 10:33:31 +0000

    no need for the addition MATLAB code, python code is updated

--- MATLAB_code_SLM/propagate.m
+++
@@ -1,7 +0,0 @@
-function [f] = propagate(f,H0)
-    %Convert real-space field, to k-space field
-    F = fftshift(fft2(fftshift(f)));
-    %Apply the transfer function of free-space
-    F = F.*H0;
-    %Convert k-space field back to real-space
-    f = fftshift(ifft2(fftshift(F)));
